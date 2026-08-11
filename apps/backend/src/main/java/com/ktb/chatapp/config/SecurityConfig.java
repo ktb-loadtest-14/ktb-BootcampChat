036e5b4c03b3fa9313e878160bf6ef597d34bc6b
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2026-08-11 20:22:25 +0900

    BCrypt에서 NoOp으로 암호화 방식 변경

--- apps/backend/src/main/java/com/ktb/chatapp/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/ktb/chatapp/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new NoOpPasswordEncoder;
+        return new NoOpPasswordEncoder();
     }
 
     @Bean
