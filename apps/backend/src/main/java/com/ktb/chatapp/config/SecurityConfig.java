95caa8b35aa7e75d2927c453bffbca5b399a33f5
LifeSatin <161478370+LifeSatin@users.noreply.github.com>
2026-08-11 20:22:13 +0900

    BCrypt에서 NoOp으로 암호화 방식 변경

--- apps/backend/src/main/java/com/ktb/chatapp/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/ktb/chatapp/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -55,7 +56,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(BCRYPT_COST);
+        return new NoOpPasswordEncoder;
     }
 
     @Bean
